5880cafdc7fdd83b11852234a8989187b644bec7
wanglidan <wangld6543@163.com>
2019-07-17 11:01:23 +0800

    初始化项目

---
+++ weapp/app.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var _core = _interopRequireDefault(require('vendor.js')(0));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+_core["default"].app({
+  globalData: {
+    userInfo: null
+  },
+  onLaunch: function onLaunch() {},
+  methods: {}
+}, {a: 1});
---
+++ weapp/pages/shop_cart.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var _core = _interopRequireDefault(require('../vendor.js')(0));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+_core["default"].page({
+  data: {},
+  computed: {},
+  methods: {},
+  created: function created() {}
+}, {info: {"components":{},"on":{}}, handlers: {}, models: {} });
